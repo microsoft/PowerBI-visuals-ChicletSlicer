5000ad91f13f56440dad87b9ed10978220ee7f83
Ignat Vilesov <v-viig@microsoft.com>
2016-10-21 20:05:38 +0300

    11160 “ChicletSlicer: Integrate InteractivityService to the visual”

--- src/enumerationWorkaround.js
+++
@@ -1,6 +0,0 @@
-powerbi.visuals.system.DebugVisual.prototype.enumerateObjectInstances = function (options) {
- if (this.adapter && this.adapter.enumerateObjectInstancesAsync) {
-  return this.adapter.enumerateObjectInstancesAsync(options);
- }
- return [];
-}
